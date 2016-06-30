9b7340bee47848fb819a7a9e97016fd7e117b632
KingRial <kingrichard1980@gmail.com>
2016-06-30 17:54:19 +0200

    Z-Wave: adding comments and documentation
    Z-Wave: adding comments and documentation
    Added new method to set values

--- integrations/Z-Wave/Technology.Z-Wave.js
+++ integrations/Z-Wave/Technology.Z-Wave.js
@@ -7,7 +7,7 @@ let Bluebird = require('bluebird');
 
 let Technology = require('../../lib/ancilla.js').Technology;
 
-let Node = require('./lib/Node.js');
+let ZWaveNode = require('./lib/Node.js');
 
 /**
  * A Technology used to connect to Z-Wave
@@ -265,7 +265,7 @@ class TechnologyZWave extends Technology {
 	addNode( oNodeInfo ){
 		let _Zwave = this;
 		let _oNode = this.getNode( oNodeInfo.node_id );
-		_oNode = ( _oNode ? _oNode : new Node() );
+		_oNode = ( _oNode ? _oNode : new ZWaveNode() );
 		_oNode.update({
 			iID: oNodeInfo.node_id,
 			sName: oNodeInfo.name,
@@ -299,6 +299,19 @@ class TechnologyZWave extends Technology {
 		return this.__oNodes;
 	}
 
+	/**
+	 * Method used to add a new node's value
+	 *
+	 * @method    addNodeValue
+	 * @public
+	 *
+	 * @param	{Number}	oValue	The node's value object
+	 *
+	 * @return	{Void}
+	 *
+	 * @example
+	 *   Zwave.addNodeValue( oFooValueObject );
+	 */
 	addNodeValue( oValue ){
 		let _Zwave = this;
 		let _iNodeID = oValue.node_id;
@@ -331,10 +344,37 @@ class TechnologyZWave extends Technology {
 		_Zwave.debug( 'Node ID: "%s" -> Valued discovered:', _oValue.getID(), _oValue );
 	}
 
-	getNodeValue( iNodeID, iValueID ){
-		return ( this.__oNodes[ iNodeID ] ? this.__oNodes[ iNodeID ].oValues[ iValueID ] : null );
+	/**
+	 * Method used to get a specific values related to a specific node
+	 *
+	 * @method    getNodeValue
+	 * @public
+	 *
+	 * @param	{Number}	iNodeID	The node ID
+	 * @param	{String}	sValueID	The value ID
+	 *
+	 * @return	{Object}	The object which represents the value
+	 *
+	 * @example
+	 *   Zwave.getNodeValue( 1, '1-34-1-1' );
+	 */
+	getNodeValue( iNodeID, sValueID ){
+		return ( this.__oNodes[ iNodeID ] ? this.__oNodes[ iNodeID ].oValues[ sValueID ] : null );
 	}
 
+	/**
+	 * Method used to get all the values related to a specific node
+	 *
+	 * @method    getNodeValues
+	 * @public
+	 *
+	 * @param	{Number}	iNodeID	The node ID
+	 *
+	 * @return	{Object}	A matrix which allow the access to all the value's objects
+	 *
+	 * @example
+	 *   Zwave.getNodeValues( 1 );
+	 */
 	getNodeValues( iNodeID ){
 		return ( this.__oNodes[ iNodeID ] ? this.__oNodes[ iNodeID ].oValues : null );
 	}
@@ -398,19 +438,67 @@ class TechnologyZWave extends Technology {
 	  });
 	}
 
+	/**
+	 * Method used to set the current OpenZWave controller ( https://github.com/OpenZWave/node-openzwave-shared )
+	 *
+	 * @method    setController
+	 * @public
+	 *
+	 * @param	{Object}	oController	The controller to be used by the techology
+	 *
+	 * @return	{Void}
+	 *
+	 * @example
+	 *   Zwave.setController( oFooController );
+	 */
 	setController( oController ){
 		this.__oController = oController;
 	}
 
+	/**
+	 * Method used to obtain the OpenZWave controller ( https://github.com/OpenZWave/node-openzwave-shared )
+	 *
+	 * @method    getController
+	 * @public
+	 *
+	 * @return	{Object}	The OpenZWave controller
+	 *
+	 * @example
+	 *   Zwave.getController();
+	 */
 	getController(){
 		return this.__oController;
 	}
 
+	/**
+	 * Method used to remember the current HomeID obtained by the controller
+	 *
+	 * @method    setHomeID
+	 * @public
+	 *
+	 * @param     {String}		sHomeID			The Home ID which has to be remembered
+	 *
+	 * @return	{Void}
+	 *
+	 * @example
+	 *   Zwave.setHomeID( 'foo' );
+	 */
 	setHomeID( sHomeID ){
 		this.debug( 'Using Home ID: "%s"...', sHomeID );
 		this.__sHomeID = sHomeID;
 	}
 
+	/**
+	 * Method used to pair a Node
+	 *
+	 * @method    pair
+	 * @public
+	 *
+	 * @return	{Object} returna a Promise
+	 *
+	 * @example
+	 *   Zwave.pair();
+	 */
 	pair( bSecure ){
 		bSecure = bSecure || false;
 		let _Zwave = this;
@@ -433,6 +521,17 @@ class TechnologyZWave extends Technology {
 		;
 	}
 
+	/**
+	 * Method used to unpair a Node
+	 *
+	 * @method    unpair
+	 * @public
+	 *
+	 * @return	{Object} returna a Promise
+	 *
+	 * @example
+	 *   Zwave.unpair();
+	 */
 	unpair(){
 		let _Zwave = this;
 		let _oController = _Zwave.getController();
@@ -455,6 +554,20 @@ class TechnologyZWave extends Technology {
 		;
 	}
 
+	/**
+	 * Method used to reset the OpenZWave controller configuration
+	 *
+	 * @method    reset
+	 * @public
+	 *
+	 * @param     {Boolean}		bHardReset			A flag which tells the controller to do an hard reset instead of a soft reset
+	 *
+	 * @return	{Object} returna a Promise
+	 *
+	 * @example
+	 *   Zwave.reset();
+	 *   Zwave.reset( true );
+	 */
 	reset( bHardReset ){
 		let _Zwave = this;
 		let _oController = _Zwave.getController();
@@ -465,6 +578,40 @@ class TechnologyZWave extends Technology {
 			_Zwave.info( 'Configuration soft reset' );
 			_oController.softReset();
 		}
+		return Promise.resolve();
+	}
+
+	/**
+	 * Method used to set a value on an open Z-Wave value
+	 *
+	 * @method    set
+	 * @public
+	 *
+	 * @param     {String/Number}		iNodeIDOrValueID			The node ID or the Value ID
+	 * @param     {Number}		iClassIDOrValue			The node value class ID or the value to set
+	 * @param     {Number}		[iInstance]			The node value instance ID
+	 * @param     {Number}		[iIndex]			The node value index ID
+	 * @param     {String/Number}		[value]			The value to set
+	 *
+	 * @return	{Object} returna a Promise
+	 *
+	 * @example
+	 *   Zwave.set( '2-37-1-0', true );
+	 *   Zwave.set( 2, 37, 1, 0, true );
+	 */
+	set( iNodeIDOrValueID, iClassIDOrValue, iInstance, iIndex, value ){
+		let _Zwave = this;
+		let _oController = _Zwave.getController();
+		if( typeof iNodeIDOrValueID ==='string' && !iInstance && !iIndex && !value){
+			let _aArguments = iNodeIDOrValueID.split('-');
+			value = iClassIDOrValue;
+			iNodeIDOrValueID = parseInt( _aArguments[ 0 ] );
+			iClassIDOrValue = parseInt( _aArguments[ 1 ] );
+			iInstance = parseInt( _aArguments[ 2 ] );
+			iIndex = parseInt( _aArguments[ 3 ] );
+		}
+		this.debug( 'setting value "%s" on "%s-%s-%s-%s"', value, iNodeIDOrValueID, iClassIDOrValue, iInstance, iIndex );
+		_oController.setValue( iNodeIDOrValueID, iClassIDOrValue, iInstance, iIndex, value );
 		return Promise.resolve();
 	}
 
