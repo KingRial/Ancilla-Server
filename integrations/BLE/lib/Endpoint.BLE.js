a56235811c9643f3309f595781917fbe8a325498
KingRial <kingrichard1980@gmail.com>
2016-12-06 11:13:27 +0100

    Updating dependacies
    Updating dependacies

--- integrations/BLE/lib/Endpoint.BLE.js
+++ integrations/BLE/lib/Endpoint.BLE.js
@@ -2,7 +2,6 @@
 
 let _ = require( 'lodash' );
 let Bluebird = require( 'bluebird' );
-let noble = require('noble');
 
 let Endpoint = require('../../../lib/ancilla.js').Endpoint;
 
@@ -32,50 +31,59 @@ class BLEEndpoint extends Endpoint {
  }
 
  __readyController(){
-  noble.on('scanStart', function(){
-    console.info( 'scanStart' );
-  });
-  noble.on('scanStop', function(){
-    console.info( 'scanStop' );
-  });
-  noble.on('discover', function( peripheral ){
-    console.info( 'found peripheral: ', peripheral );
-  });
-  noble.on('warning', function( message ){
-    console.info('Warning: ', message );
-  });
-  /*
-  peripheral.once('connect', callback);
-  peripheral.once('disconnect', callback);
-  peripheral.once('rssiUpdate', callback(rssi));
-  peripheral.once('servicesDiscover', callback(services));
-  service.once('includedServicesDiscover', callback(includedServiceUuids));
-  service.once('characteristicsDiscover', callback(characteristics));
-  characteristic.on('data', callback(data, isNotification));
-  characteristic.once('read', callback(data, isNotification)); // legacy
-  characteristic.once('write', withoutResponse, callback());
-  characteristic.once('broadcast', callback(state));
-  characteristic.once('notify', callback(state));
-  characteristic.once('descriptorsDiscover', callback(descriptors));
-  descriptor.once('valueRead', data);
-  descriptor.once('valueWrite');
-  */
-  //noble.startScanning( [], false ); // any service UUID, no duplicates
-  //
-  let _Endpoint = this;
-  return new Bluebird( function( fResolve ){
-    _Endpoint.debug( 'connecting to USB Enocean controller: "%s"', _Endpoint.getConfig().sUSBController );
-    noble.on('stateChange', function( state ){
-      console.info( 'stateChange: ', state );
-      if (state === 'poweredOn') {
-        noble.startScanning( [], false ); // any service UUID, no duplicates
-        fResolve();
-      } else {
-        noble.stopScanning();
-      }
-    });
-  });
-  }
+   try{
+     let noble = require( 'noble' );
+     noble.on('scanStart', function(){
+       console.info( 'scanStart' );
+     });
+     noble.on('scanStop', function(){
+       console.info( 'scanStop' );
+     });
+     noble.on('discover', function( peripheral ){
+       console.info( 'found peripheral: ', peripheral );
+     });
+     noble.on('error', function( error ){
+       console.info('Error: ', error );
+     });
+     noble.on('warning', function( message ){
+       console.info('Warning: ', message );
+     });
+     /*
+     peripheral.once('connect', callback);
+     peripheral.once('disconnect', callback);
+     peripheral.once('rssiUpdate', callback(rssi));
+     peripheral.once('servicesDiscover', callback(services));
+     service.once('includedServicesDiscover', callback(includedServiceUuids));
+     service.once('characteristicsDiscover', callback(characteristics));
+     characteristic.on('data', callback(data, isNotification));
+     characteristic.once('read', callback(data, isNotification)); // legacy
+     characteristic.once('write', withoutResponse, callback());
+     characteristic.once('broadcast', callback(state));
+     characteristic.once('notify', callback(state));
+     characteristic.once('descriptorsDiscover', callback(descriptors));
+     descriptor.once('valueRead', data);
+     descriptor.once('valueWrite');
+     */
+     //noble.startScanning( [], false ); // any service UUID, no duplicates
+     //
+     let _Endpoint = this;
+     return new Bluebird( function( fResolve ){
+       _Endpoint.debug( 'connecting to USB Enocean controller: "%s"', _Endpoint.getConfig().sUSBController );
+       noble.on('stateChange', function( state ){
+         console.info( 'stateChange: ', state );
+         if (state === 'poweredOn') {
+           noble.startScanning( [], false ); // any service UUID, no duplicates
+           fResolve();
+         } else {
+           noble.stopScanning();
+         }
+       });
+     });
+   }catch( e ){
+     this.error( 'Unable to start endpoint: ', e );
+     return Bluebird.reject( e );
+   }
+ }
 
 }
 
